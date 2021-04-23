193c06bc8c88b1a995ea4a2a83d619e74214b712
HelenLindberg <helenilindberg@yahoo.com>
2021-04-22 21:45:47 -0500

    server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+require('dotenv').config()
 
 const PORT = process.env.PORT || 3025;
 
@@ -16,7 +17,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URL || 'mongodb://localhost/budget',
+  process.env.MONGODB_URI || 'mongodb://localhost/budget',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -25,7 +26,6 @@ mongoose.connect(
   }
 );
 
-// routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
